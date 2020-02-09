91372bef1a7c88a13705b057c157ecce4b121869
Juan P. Martos <kertacho@gmail.com>
2020-02-09 17:33:33 +0100

    Add files via upload

---
+++ interfaces/InterfaceExample.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Unit10ExA;
+
+/**
+ *
+ * @author IES TRASSIERRA
+ */
+public class InterfaceExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
